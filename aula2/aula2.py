8aa0ec700a65d552c1b4327f8128a3a670fa89a3
laranjaaa00 <124391657+laranjaaa00@users.noreply.github.com>
2024-12-02 10:02:40 -0300

    aprendi variavel e print

---
+++ aula2/aula2.py
@@ -0,0 +1,11 @@
+print("ola, mundo!")
+
+nome_usuario = "nicolas" #tipo -> = string
+idade_usuario = 17
+altura_usuario = 1.69 
+maior_de_idade = False #tipo -> bool = boolean 
+print(nome_usuario)
+print(altura_usuario)
+print(maior_de_idade)
+
+print("seja bem-vindo", nome_usuario, "sua idade é", idade_usuario, "sual altura é", altura_usuario, "voce é maior de idade?", maior_de_idade)
